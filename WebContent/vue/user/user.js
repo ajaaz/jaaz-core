651e19da0aa33e6c84b9134d1267e34b1bc04d9b
slu <slu@user-HP>
2018-01-06 17:47:07 +0100

    implemented login view

--- WebContent/vue/user/user.js
+++ WebContent/vue/user/user.js
@@ -1,3 +1,8 @@
 Vue.component('user-menu', {
-	 template: '#user-menu-template'
+	 template: '#user-menu-template',
+	  methods: {
+		    openLoginWindow: function () {
+		     $("#user-login-window").modal("toggle");
+		    }
+		  }
 })
